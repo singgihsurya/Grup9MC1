c2d85a0dbb12e2695a5c6cc2807043583c3c47af
Singgih <singgihsurya91@gmail.com>
2020-04-02 15:05:54 +0700

    Initial Commit

---
+++ Grup9MC1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Grup9MC1
+//
+//  Created by Singgih Surya Dharma on 02/04/20.
+//  Copyright © 2020 Singgih Surya Dharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
