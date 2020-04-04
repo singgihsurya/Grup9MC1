23a7ee4abc91376a9f42f5487fc916e2d061b812
Bryanza <bryanza@Bryanzas-MacBook-Air.local>
2020-04-05 01:30:38 +0700

    Initial food venture page

---
+++ Grup9MC1/Foodventure.swift
@@ -0,0 +1,14 @@
+//
+//  Foodventure.swift
+//  Grup9MC1
+//
+//  Created by Bryanza on 05/04/20.
+//  Copyright © 2020 Singgih Surya Dharma. All rights reserved.
+//
+
+//import Foundation
+
+struct Foodventures {
+    var username: String
+    var progress: Int
+}
---
+++ Grup9MC1/FoodventureViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FoodventureViewController.swift
+//  Grup9MC1
+//
+//  Created by Bryanza on 05/04/20.
+//  Copyright © 2020 Singgih Surya Dharma. All rights reserved.
+//
+
+//import Foundation
+
+import UIKit
+
+class FoodventureViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
