d78c748dd06b88c11298ab99607059cddb5e2226
Peter Liuwandy <peterliuwandy@gmail.com>
2020-04-07 22:08:19 +0700

    Onboarding page template (minus button)

---
+++ Grup9MC1/OnboardingViewController.swift
@@ -0,0 +1,130 @@
+//
+//  OnboardingViewController.swift
+//  Grup9MC1
+//
+//  Created by Peter Liuwandy on 07/04/20.
+//  Copyright © 2020 Singgih Surya Dharma. All rights reserved.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+
+    @IBOutlet weak var contentView: UIView!
+    
+    let onboardsLabel = ["Halaman 1", "Halaman 2", "Halaman 3"]
+    
+    let onboardsImage = ["Char1Red", "Char1Green", "Char1Yellow"]
+    
+    var currentViewControllerIndex = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configurePageViewController()
+        // Do any additional setup after loading the view.
+    }
+    
+    func configurePageViewController(){
+        
+        guard let pageViewController = storyboard?.instantiateViewController(identifier: String(describing: CustomPageViewController.self)) as? CustomPageViewController else{
+            return
+        }
+        
+        pageViewController.delegate = self
+        pageViewController.dataSource = self
+        
+        addChild(pageViewController)
+        pageViewController.didMove(toParent: self)
+        
+        pageViewController.view.translatesAutoresizingMaskIntoConstraints = false
+        
+        contentView.addSubview(pageViewController.view)
+        
+        let views: [String: Any] = ["pageView": pageViewController.view ?? ""]
+        
+        contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-0-[pageView]-0-|", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: views))
+        
+        contentView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-0-[pageView]-0-|", options: NSLayoutConstraint.FormatOptions(rawValue: 0), metrics: nil, views: views))
+        
+        guard let startingViewController = detailViewControllerAt(index: currentViewControllerIndex) else{
+            return
+        }
+        
+        pageViewController.setViewControllers([startingViewController], direction: .forward, animated: true)
+    }
+    
+    func detailViewControllerAt(index: Int) -> DataViewController? {
+        
+        if index >= onboardsLabel.count || onboardsLabel.count == 0{
+            return nil
+        }
+           
+           guard let dataViewController = storyboard?.instantiateViewController(identifier: String(describing: DataViewController.self)) as? DataViewController else{
+               return nil
+           }
+        dataViewController.index = index
+        dataViewController.displayText = onboardsLabel[index]
+        dataViewController.displayImage = onboardsImage[index]
+           return dataViewController
+       }
+       
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+extension OnboardingViewController: UIPageViewControllerDelegate, UIPageViewControllerDataSource{
+    
+    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        return currentViewControllerIndex
+    }
+    
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return onboardsLabel.count
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        let dataViewController = viewController as? DataViewController
+        
+        guard var currentIndex = dataViewController?.index else{
+            return nil
+        }
+        
+        currentViewControllerIndex = currentIndex
+        
+        if currentIndex == 0 {
+            return nil
+        }
+        
+        currentIndex -= 1
+        
+        return detailViewControllerAt(index: currentIndex)
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        let dataViewController = viewController as? DataViewController
+        
+        guard var currentIndex = dataViewController?.index else{
+            return nil
+        }
+        
+        if currentIndex == onboardsImage.count{
+            return nil
+        }
+        
+        currentIndex += 1
+        
+        currentViewControllerIndex = currentIndex
+        
+        return detailViewControllerAt(index: currentIndex)
+        
+    }
+}
